dda7329e05d316074a98e84d650e1ac2f8500753
TianSeptiandi <153967315+TianSeptiandi@users.noreply.github.com>
2026-05-01 17:16:05 +0700

    Add files via upload

---
+++ dosen.js
@@ -0,0 +1,35 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.12.1/firebase-app.js";
+import {getDatabase,ref,push,onValue} from "https://www.gstatic.com/firebasejs/12.12.1/firebase-database.js";
+
+const db = getDatabase (initializeApp({
+      apiKey: "AIzaSyBH9fdaDioSIBohv04Nwn5UsB-Wh8Q8AaU",
+    authDomain: "latihan2-ba4c5.firebaseapp.com",
+    projectId: "latihan2-ba4c5",
+    storageBucket: "latihan2-ba4c5.firebasestorage.app",
+    messagingSenderId: "515994512533",
+    appId: "1:515994512533:web:7bdd166157ac92f398ff9c"
+
+}));
+
+const nama = document.getElementById("nama");
+const alamat = document.getElementById("alamat");
+const ttl = document.getElementById("ttl");
+const simpan = document.getElementById("simpan");
+const daftar = document.getElementById("daftar");
+
+const dataref = ref(db,"dosen");
+
+simpan.onclick = () =>{
+    
+ // console.log(nama.value,nama2.value);
+
+push (dataref,{
+   nama : nama.value,
+   alamat : alamat.value,
+   ttl : ttl.value,
+});
+//console.log();
+   nama.value = "";
+   alamat.value="";
+   ttl.value=";"
+};
---
+++ index.js
@@ -0,0 +1,32 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.12.1/firebase-app.js";
+import {getDatabase,ref,push,onValue} from "https://www.gstatic.com/firebasejs/12.12.1/firebase-database.js";
+
+const db = getDatabase (initializeApp({
+      apiKey: "AIzaSyBH9fdaDioSIBohv04Nwn5UsB-Wh8Q8AaU",
+    authDomain: "latihan2-ba4c5.firebaseapp.com",
+    projectId: "latihan2-ba4c5",
+    storageBucket: "latihan2-ba4c5.firebasestorage.app",
+    messagingSenderId: "515994512533",
+    appId: "1:515994512533:web:7bdd166157ac92f398ff9c"
+
+}));
+
+const nama = document.getElementById("nama");
+const alamat = document.getElementById("alamat");
+const simpan = document.getElementById("simpan");
+const daftar = document.getElementById("daftar");
+
+const dataref = ref(db,"mahasiswa");
+
+simpan.onclick = () =>{
+    
+ // console.log(nama.value,nama2.value);
+
+push (dataref,{
+   nama : nama.value,
+   nama2 : alamat.value,
+});
+//console.log();
+   nama.value = "";
+   alamat.value=";"
+};
